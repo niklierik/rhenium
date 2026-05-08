c62d9ac3457750a6855e9cbb940384647b61f56e
niklierik <erik200203@gmail.com>
2026-05-08 21:48:26 +0200

    Init project and its modules

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+dependencyResolutionManagement {
+    // Use Maven Central as the default repository (where Gradle will download dependencies) in all subprojects.
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+include(":app")
+include(":common")
+
+rootProject.name = "rhenium"
+include("parser")
+include("ast-builder")
+include("semantic-analyzer")
+include("transpiler")
